f1cd775a3d4288bc61b192eba0a66cf86518d907
Tobias Nickel <business@tnickel.de>
2017-09-13 21:57:55 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # trsa
-node-forge rsa with an API that you understand
+node-forge rsa with an API that you understand.
+
+node-forge is a great module, it offers good encryption. Sadly its API is to complex. This module reduce the API for rsa signing/veriication and encryption/decryption to a minimum of just 6 functions.
+
+ - generateKeyPair() and  generaitKeyPairSync() to create public and private key
+ - sign(data, privateKey) to sign data
+ - verify(data, publicKey) to verify the source of the data
+ - encrypt(data, publicKey) to encrypt something that only the coresponding privateKey can decrypt
+ - decrypt(data, privateKey) decrypt data that was encrypted specially for you.
+
+you can use webpack or browserify to use the rsa module in the browser.
+
---
+++ rsa.js
@@ -0,0 +1,71 @@
+/**
+ * I saw the node-forge API is very uglu, the rsa module will just provide a nicer API.
+ * - tasks should be possible to finish in one call
+ * - async functions should return a promise
+ * - keys, signatures and encrypted data should be represented stringifyable data not as complex objects
+ */
+
+var forge = require('node-forge');
+
+var rsa = forge.pki.rsa;
+
+function generateKeyPair(options) {
+    if (!options) options = {};
+    return new Promise((resolve, reject) => {
+        if (!options.bits) options.bits = 2048; // 4096;
+        if (!options.workers) options.workers = 2;
+
+        // options is like: { bits: 2048, workers: 2 }
+        rsa.generateKeyPair(options, function(err, { privateKey, publicKey }) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            // keypair.privateKey, keypair.publicKey 
+            privateKey = forge.pki.privateKeyToPem(privateKey);
+            publicKey = forge.pki.publicKeyToPem(publicKey);
+            // forge.pki.privateKeyFromPem(forge.pki.privateKeyToPem(m.keypair.privateKey));
+            // forge.pki.publicKeyFromPem(forge.pki.publicKeyToPem(m.keypair.publicKey))
+            resolve({ privateKey, publicKey });
+        });
+    });
+}
+
+
+const generateKeyPairSync = rsa.generateKeyPair;
+
+function sign(data, privateKey) {
+    const md = forge.md.sha1.create();
+    privateKey = forge.pki.privateKeyFromPem(privateKey);
+    md.update(data, 'utf8');
+    const signature = privateKey.sign(md);
+    return signature; //JSON.stringify(signature) //new Buffer(signature).toString('base64');
+}
+
+function verify(data, signature, publicKey) {
+    //signature = JSON.parse(signature) //new Buffer(signature, 'base64').toString('ascii')
+    publicKey = forge.pki.publicKeyFromPem(publicKey);
+    const md = forge.md.sha1.create();
+    md.update(data, 'utf8');
+    const bytes = md.digest().bytes()
+    return publicKey.verify(bytes, signature);
+}
+
+function encrypt(data, publicKey) {
+    publicKey = forge.pki.publicKeyFromPem(publicKey);
+    return publicKey.encrypt(data);
+}
+
+function decrypt(data, privateKey) {
+    privateKey = forge.pki.privateKeyFromPem(privateKey);
+    return privateKey.decrypt(data);
+}
+
+module.exports = {
+    generateKeyPair,
+    generateKeyPairSync,
+    sign,
+    verify,
+    encrypt,
+    decrypt,
+};
